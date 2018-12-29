acf88be5cb8ea3e24b38a1eabb9551e620cc05e6
Tom Tarpey <tomtarpey@hotmail.com>
2018-12-29 16:05:36 +0000

    started to add some scripts

---
+++ tarbs.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+# Tom's Auto Rice Boostrapping Script (TARBS)
+# by Tom Tarpey <tomtarpey@hotmail.com>
+# License: MIT
+
+# You can provide a custom repository with -r or a custom programs csv with -p.
+# Otherwise, the script will use my defaults.
+
+###
+### OPTIONS AND VARIABLES ###
+###
+
+while getopts ":a:r:p:h" o; do case "${o}" in
+	h) echo -e "Optional arguments for custom use:\\n  -r: Dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message" && exit ;;
+	r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit ;;
+	p) progsfile=${OPTARG} ;;
+	a) aurhelper=${OPTARG} ;;
+	*) echo "-$OPTARG is not a valid option." && exit ;;
+esac done
+
+# DEFAULTS:
+[ -z ${dotfilesrepo+x} ] && dotfilesrepo="https://github.com/tomtarpey/dotrice.git"
+[ -z ${progsfile+x} ] && progsfile="https://raw.githubusercontent.com/tomtarpey/TARBS/master/progs.csv"
+[ -z ${aurhelper+x} ] && aurhelper="yay"
+
+###
+### FUNCTIONS ###
+###
+
+initialcheck() { pacman -Syyu --noconfirm --needed dialog || { echo "Are you sure you're running this as the root user? Are you sure you're using an Arch-based distro? ;-) Are you sure you have an internet connection? Are you sure your Arch keyring is updated?"; exit; } ;}
+
+preinstallmsg() { \
+	dialog --title "Let's get started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit; }
+	}
+
+welcomemsg() { \
+	dialog --title "Welcome!" --msgbox "Welcome to Tom's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured i3wm Arch Linux desktop.\\n\\n-Tom" 10 60
+	}
+
+refreshkeys() { \
+	dialog --infobox "Refreshing Arch Keyring..." 4 40
+	pacman --noconfirm -Sy archlinux-keyring &>/dev/null
+	}
+
+getuserandpass() { \
+	# Prompts user for new username an password.
+	# Checks if username is valid and confirms passwd.
+	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+	namere="^[a-z_][a-z0-9_-]*$"
+	while ! [[ "${name}" =~ ${namere} ]]; do
+		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done
+	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
+	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	while ! [[ ${pass1} == "${pass2}" ]]; do
+		unset pass2
+		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done ;}
+
+usercheck() { \
+	! (id -u "$name" &>/dev/null) ||
+	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. TARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that TARBS will change $name's password to the one you just gave." 14 70
+	}
+
+adduserandpass() { \
+	# Adds user `$name` with password $pass1.
+	dialog --infobox "Adding user \"$name\"..." 4 50
+	useradd -m -g wheel -s /bin/bash "$name" &>/dev/null ||
+	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+	echo "$name:$pass1" | chpasswd
+	unset pass1 pass2 ;}
+
+gitmakeinstall() {
+	dir=$(mktemp -d)
+	dialog --title "TARBS Installation" --infobox "Installing \`$(basename "$1")\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
+	git clone --depth 1 "$1" "$dir" &>/dev/null
+	cd "$dir" || exit
+	make &>/dev/null
+	make install &>/dev/null
+	cd /tmp || return ;}
+
+maininstall() { # Installs all needed programs from main repo.
+	dialog --title "TARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
+	pacman --noconfirm --needed -S "$1" &>/dev/null
+	}
+
+aurinstall() { \
+	dialog --title "TARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
+	grep "^$1$" <<< "$aurinstalled" && return
+	sudo -u "$name" $aurhelper -S --noconfirm "$1" &>/dev/null
+	}
+
+installationloop() { \
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
+	total=$(wc -l < /tmp/progs.csv)
+	aurinstalled=$(pacman -Qm | awk '{print $1}')
+	while IFS=, read -r tag program comment; do
+	n=$((n+1))
+	echo "$comment" | grep "^\".*\"$" >/dev/null && comment="$(echo "$comment" | sed "s/\(^\"\|\"$\)//g")"
+	case "$tag" in
+	"") maininstall "$program" "$comment" ;;
+	"A") aurinstall "$program" "$comment" ;;
+	"G") gitmakeinstall "$program" "$comment" ;;
+	esac
+	done < /tmp/progs.csv ;}
+
+serviceinit() { for service in "$@"; do
+	dialog --infobox "Enabling \"$service\"..." 4 40
+	systemctl enable "$service"
+	systemctl start "$service"
+	done ;}
+
+newperms() { # Set special sudoers settings for install (or after).
+	sed -i "/#TARBS/d" /etc/sudoers
+	echo -e "$@ #TARBS" >> /etc/sudoers ;}
+
+systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
+	rmmod pcspkr
+	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
+
+putgitrepo() { # Downlods a gitrepo $1 and places the files in $2 only overwriting conflicts
+	dialog --infobox "Downloading and installing config files..." 4 60
+	dir=$(mktemp -d)
+	chown -R "$name":wheel "$dir"
+	sudo -u "$name" git clone --depth 1 "$1" "$dir"/gitrepo &>/dev/null &&
+	sudo -u "$name" mkdir -p "$2" &&
+	sudo -u "$name" cp -rT "$dir"/gitrepo "$2"
+	}
+
+resetpulse() { dialog --infobox "Reseting Pulseaudio..." 4 50
+	killall pulseaudio
+	sudo -n "$name" pulseaudio --start ;}
+
+manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
+	[[ -f /usr/bin/$1 ]] || (
+	dialog --infobox "Installing \"$1\", an AUR helper..." 4 50
+	cd /tmp || exit
+	rm -rf /tmp/"$1"*
+	curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/"$1".tar.gz &&
+	sudo -u "$name" tar -xvf "$1".tar.gz &>/dev/null &&
+	cd "$1" &&
+	sudo -u "$name" makepkg --noconfirm -si &>/dev/null
+	cd /tmp || return) ;}
+
+finalize(){ \
+	dialog --infobox "Preparing welcome message..." 4 50
+	echo "exec_always --no-startup-id notify-send -i ~/.scripts/pix/larbs.png '<b>Hello Tom:</b> Do you want to play a game?' -t 10000"  >> "/home/$name/.config/i3/config"
+	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Tom" 12 80
+	}
+
+###
+### THE ACTUAL SCRIPT ###
+###
+### This is how everything happens in an intuitive format and order.
+###
+
+# Check if user is root on Arch distro. Install dialog.
+initialcheck
+
+# Welcome user.
+welcomemsg || { clear; exit; }
+
+# Get and verify username and password.
+getuserandpass
+
+# Give warning if user already exists.
+usercheck || { clear; exit; }
+
+# Last chance for user to back out before install.
+preinstallmsg || { clear; exit; }
+
+### The rest of the script requires no user input.
+
+adduserandpass
+
+# Refresh Arch keyrings.
+refreshkeys
+
+# Allow user to run sudo without password. Since AUR programs must be installed
+# in a fakeroot environment, this is required for all builds with AUR.
+newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
+
+dialog --title "TARBS Installation" --infobox "Installing \`basedevel\` and \`git\` for installing other software." 5 70
+pacman --noconfirm --needed -S base-devel git &>/dev/null
+
+manualinstall $aurhelper
+
+# The command that does all the installing. Reads the progs.csv file and
+# installs each needed program the way required. Be sure to run this only after
+# the user has been created and has priviledges to run sudo without a password
+# and all build dependencies are installed.
+installationloop
+
+# Install the dotfiles in the user's home directory
+putgitrepo "$dotfilesrepo" "/home/$name"
+
+# Install the TARBS Firefox profile in ~/.mozilla/firefox/
+putgitrepo "https://github.com/tomtarpey/mozi.git" "/home/$name/.mozilla/firefox"
+
+# Pulseaudio, if/when initially installed, often needs a restart to work immediately.
+[[ -f /usr/bin/pulseaudio ]] && resetpulse
+
+# Install vim `plugged` plugins.
+dialog --infobox "Installing vim plugins..." 4 50
+sudo -u "$name" vim -E -c "PlugUpdate|visual|q|q" >/dev/null
+
+# Enable services here.
+serviceinit NetworkManager cronie
+
+# Most important command! Get rid of the beep!
+systembeepoff
+
+# This line, overwriting the `newperms` command above will allow the user to run
+# serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
+newperms "%wheel ALL=(ALL) ALL\\n%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"
+
+# Make pacman and yay colorful because why not.
+sed -i "s/^#Color/Color/g" /etc/pacman.conf
+
+# Last message! Install complete!
+finalize
+clear
